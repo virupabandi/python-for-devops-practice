084332d827ec47cbd14c59fce9c3973c475f27bc
virupabandi <89890365+virupabandi@users.noreply.github.com>
2024-06-08 01:20:57 +0000

    integration project

--- Day-14/examples/list_projects.py
+++ Day-14/examples/list_projects.py
@@ -4,11 +4,11 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://veeramallaabhishek.atlassian.net/rest/api/3/project"
+url = "https://veerupakshabandi.atlassian.net/rest/api/3/project"
 
-API_TOKEN=""
+API_TOKEN="ATATT3xFfGF0faV9yWN_SrNwztQLekG9kVqeuihogAu0i7LYyhRXKULDmYgvpSq_Bl30BslrJRci4qQqAh3Dvq8yK6crLNlRtLCDyo7dcc49wuksApOGvxZelqXOOKqs2H1C09_Do39NJjMSOp1YhAfjKXUQxmPiRfch7RW0WHgu_uVB8Yew37s=147BFD0D"
 
-auth = HTTPBasicAuth("", API_TOKEN)
+auth = HTTPBasicAuth("veerupaksha.bandi@gmail.com", API_TOKEN)
 
 headers = {
   "Accept": "application/json"
@@ -21,6 +21,11 @@ response = requests.request(
    auth=auth
 )
 
+# To print output in the json format please uncomment the below line
+
+# print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+
+# To Print project name.
 output = json.loads(response.text)
 
 name = output[0]["name"]
