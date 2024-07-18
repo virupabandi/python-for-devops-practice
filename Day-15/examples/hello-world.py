8ca862d69574f30001035f466ebb760b0d05457d
virupabandi <89890365+virupabandi@users.noreply.github.com>
2024-07-18 11:46:12 +0000

    15

--- Day-15/examples/hello-world.py
+++ Day-15/examples/hello-world.py
@@ -1,10 +1,81 @@
+# flast comes with an inbuilt server
+# Importing a Flask module from flask package
+#from flask import Flask
+
+# Create a flask application instance
+#app = Flask(__name__)
+
+# defination funtion, before execution of any function we should use the decorator. 
+# what is decorator, decorator is used to call the perticular action befor the function.
+# Decorator starts with @
+
+#@app.route('/')
+#def hello_world():
+#    return 'Hello, World!'
+
+#if __name__ == '__main__':
+#    app.run("0.0.0.0")
+
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+#@app.route("/")
+#def hello():
+@app.route("/createJIRA". methods=[POST])
+def createJIRA():
+    import requests
+    from requests.auth import HTTPBasicAuth
+    import json
 
-if __name__ == '__main__':
-    app.run("0.0.0.0")
+    url = "https://url.atlassian.net/rest/api/3/issue"
+
+    auth = HTTPBasicAuth("example@example.com", "api_token")
+
+    headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+    }
+
+    payload = json.dumps( {
+    "fields": {
+        "description": {
+        "content": [
+            {
+            "content": [
+                {
+                "text": "My first Jira ticket.",
+                "type": "text"
+                }
+            ],
+            "type": "paragraph"
+            }
+        ],
+        "type": "doc",
+        "version": 1
+        },
+        "issuetype": {
+        "id": "10001"
+        },
+        "project": {
+        "key": "SCRUM"
+        },
+        "summary": "First JIRA Ticket",
+    },
+    "update": {}
+    } )
+
+    response = requests.request(
+    "POST",
+    url,
+    data=payload,
+    headers=headers,
+    auth=auth
+    )
+
+    return(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+  
+
+app.run('0.0.0.0', port=5000)
